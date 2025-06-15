b3dcb1b49cf5f80c5fe3da7d6ce01fb28857b43b
Saswata Das <dassaswwata1996@gmail.com>
2025-06-15 14:00:47 +0530

    ui skeleton

--- src/_components/SuggestionsPanel.tsx
+++ src/_components/SuggestionsPanel.tsx
@@ -5,13 +5,27 @@ import SuggestionsPanelSkeleton from "./loaders/SuggestionsPanelSkeleton"
 import ReactQueryProvider from "./ReactQueryProvider"
 import { TSuggestion } from "@/common.types"
 import Link from "next/link"
+import styles from "./components.module.css";
+import { useState } from "react"
 
 function WrappedSuggestionsPanel() {
     const { isLoading, error, data } = useQuery({
         queryKey: ['suggestion-panel-recipes'],
-        queryFn: () => fetch('https://dummyjson.com/recipes?limit=10&skip=6&select=name,image').then(res => res.json()),
+        queryFn: () => fetch(`https://dummyjson.com/recipes?limit=12&select=name,image`).then(res => res.json()),
         staleTime: 3600 * 1000,
     })
+
+    const [pageSkip, setPageSkip] = useState(0);
+
+    const onClickChevronNext = () => {
+        if (pageSkip === 8) return;
+        setPageSkip(pageSkip + 4);
+    }
+
+    const onClickChevronPrev = () => {
+        if (pageSkip === 0) return;
+        setPageSkip(pageSkip - 4);
+    }
 
     if (isLoading) {
         return <SuggestionsPanelSkeleton />
@@ -22,13 +36,15 @@ function WrappedSuggestionsPanel() {
     }
 
     return (
-        <div>
+        <div className={styles.suggestionsPanel}>
+            <button className={styles.suggestionsPanelChevrons} onClick={onClickChevronPrev} disabled={pageSkip === 0}>&#11207;</button>
             {
-                data?.recipes.map((suggestion:TSuggestion) =>
-                <div key={suggestion.id}>
+                data?.recipes.slice(pageSkip, pageSkip+4).map((suggestion:TSuggestion) =>
+                <div key={suggestion.id} className={styles.suggestionCard}>
                     <Link href={`/recipe/${suggestion.id}`}>{suggestion.name}</Link>
                 </div>)
             }
+            <button className={styles.suggestionsPanelChevrons} onClick={onClickChevronNext} disabled={pageSkip === 8}>&#11208;</button>
         </div>
     )
 }
