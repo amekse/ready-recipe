ee79fa88fdafd0f16dcad4c9084c934333d4ce0e
Saswata Das <dassaswwata1996@gmail.com>
2025-06-15 12:52:21 +0530

    add recipe data

--- src/app/recipe/[id]/page.tsx
+++ src/app/recipe/[id]/page.tsx
@@ -1,16 +1,46 @@
 'use client'
 
-import { useRouter } from "next/router"
 import { use } from "react";
 import styles from "../../page.module.css";
+import ReactQueryProvider from "@/_components/ReactQueryProvider";
+import { useQuery } from "@tanstack/react-query";
 
-export default function Recipe({ params }: { params: Promise<{ id: string }> }) {
+function WrappedRecipe({ id }: { id: string }) {
+    const { data, isLoading, error } = useQuery({
+        queryKey: ['recipe-details', id],
+        queryFn: () => fetch(`https://dummyjson.com/recipes/${id}`).then(res => res.json())
+    })
+
+    if (isLoading) {
+        return (
+            <div className={styles.page}>
+                <span>Loading...</span>
+            </div>
+        )
+    }
+
+    if (error) {
+        return (
+            <div className={styles.page}>
+                <span>Sorry, couldn't get the recipe. Please try again later.</span>
+            </div>
+        )
+    }
+
+    return (
+        <div className={styles.page}>
+            <span>{JSON.stringify(data)}</span>
+        </div>
+    )
+}
+
+export default function Recipe({params}: {params: Promise<{id:string}>}) {
     const resolvedParams = use(params);
     const { id } = resolvedParams;
 
     return (
-        <div className={styles.page}>
-            <span>Recipe {id}</span>
-        </div>
+        <ReactQueryProvider>
+            <WrappedRecipe id={id} />
+        </ReactQueryProvider>
     )
 }
